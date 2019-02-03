876a57bb710391fed4630709af649410c4670023
Michael Preston <michael.n.preston@gmail.com>
2019-02-02 22:50:02 -0600

    Added main pages

---
+++ src/components/landing.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Full Stack Developer</h1>
+				<h2>Michael Preston</h2>
+			</div>
+		);
+	}
+}
+
+export default Landing;
