7c69689655a17358f8f7a5750229d2ab1e2fda1f
Michael Preston <michael.n.preston@gmail.com>
2019-02-03 23:10:19 -0600

    removed old comments

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -27,18 +27,3 @@ class Projects extends Component {
 }
 
 export default Projects;
-/**
- * 
- * <ul>
-                    href:"https://mikepreston17.github.io/GifTastic/", name: 'Gif-Tastic
-                    href:"https://github.com/MikePreston17/DesignPatterns", name: 'Design Patterns
-                    href:"https://github.com/MikePreston17/DynamicXml", name: 'XML To POCOs
-                    href:"https://mikepreston17.github.io/RPS-Multiplayer/" , name: 'RPS-Multiplayer
-                    href:"https://mikepreston17.github.io/LocoMoment/" , name: 'Loco-Moment
-                    href:"https://github.com/MikePreston17/liri-node-app/" , name: 'LIRI Bot
-                    href:"https://github.com/MikePreston17/bamazon/" , name: 'Bamazon
-                    href:"https://agile-woodland-60817.herokuapp.com/" , name: 'Burger
-                    href:"https://desolate-falls-20937.herokuapp.com/", name: 'Friend Finder
-                </ul>
- * 
- */
