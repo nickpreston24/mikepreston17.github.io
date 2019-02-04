de037dc8d9016a2beaf6c415993e3ddd48095fa6
Michael Preston <michael.n.preston@gmail.com>
2019-02-03 21:30:50 -0600

    refactored to be functional component

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 
-class Contact extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Contact Me</h1>
-            </div>
-        );
-    }
-}
+const Contact = props => (
+	<div>
+		<h1>Contact Me</h1>
+	</div>
+);
 
 export default Contact;
