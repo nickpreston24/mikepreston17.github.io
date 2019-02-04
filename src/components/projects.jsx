8ce986e48ac5f8b0be8d5e651b3e3551de5c065a
Michael Preston <michael.n.preston@gmail.com>
2019-02-03 21:33:16 -0600

    moved links to projects.jsx

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,12 +2,43 @@ import React, { Component } from 'react';
 
 class Projects extends Component {
 	render() {
+		var links = [
+			{ href: 'https://mikepreston17.github.io/GifTastic/', name: 'Gif-Tastic' },
+			{ href: 'https://github.com/MikePreston17/DynamicXml', name: 'XML To POCOs' },
+			{ href: 'https://github.com/MikePreston17/DesignPatterns', name: 'Design Patterns' },
+			{ href: 'https://mikepreston17.github.io/RPS-Multiplayer/', name: 'RPS-Multiplayer' },
+			{ href: 'https://mikepreston17.github.io/LocoMoment/', name: 'Loco-Moment' },
+			{ href: 'https://github.com/MikePreston17/liri-node-app/', name: 'LIRI Bot' },
+			{ href: 'https://github.com/MikePreston17/bamazon/', name: 'Bamazon' },
+			{ href: 'https://agile-woodland-60817.herokuapp.com/', name: 'Burger' },
+			{ href: 'https://desolate-falls-20937.herokuapp.com/', name: 'Friend Finder' },
+		];
+
 		return (
-			<div>
-				<h1>Projects</h1>
-			</div>
+			<ul>
+				{links.map(link => (
+					<li>
+						<a href={link.href}>{link.name}</a>
+					</li>
+				))}
+			</ul>
 		);
 	}
 }
 
 export default Projects;
+/**
+ * 
+ * <ul>
+                    href:"https://mikepreston17.github.io/GifTastic/", name: 'Gif-Tastic
+                    href:"https://github.com/MikePreston17/DesignPatterns", name: 'Design Patterns
+                    href:"https://github.com/MikePreston17/DynamicXml", name: 'XML To POCOs
+                    href:"https://mikepreston17.github.io/RPS-Multiplayer/" , name: 'RPS-Multiplayer
+                    href:"https://mikepreston17.github.io/LocoMoment/" , name: 'Loco-Moment
+                    href:"https://github.com/MikePreston17/liri-node-app/" , name: 'LIRI Bot
+                    href:"https://github.com/MikePreston17/bamazon/" , name: 'Bamazon
+                    href:"https://agile-woodland-60817.herokuapp.com/" , name: 'Burger
+                    href:"https://desolate-falls-20937.herokuapp.com/", name: 'Friend Finder
+                </ul>
+ * 
+ */
