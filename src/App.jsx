bda2c61b6aa3016e1aeed0cd5836011280878954
Michael Preston <michael.n.preston@gmail.com>
2019-03-04 21:06:54 -0600

    moved components around; Portfolio now one component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Layout, Header, Footer, FooterSection, FooterLinkList, Navigation, Drawer, Content } from 'react-mdl';
-import Main from './components/main';
-import { Link } from 'react-router-dom';
-import Resume from './components/resume';
+import Portfolio from './pages/Portfolio';
 
 class App extends Component {
 	render() {
@@ -13,38 +10,9 @@ class App extends Component {
 				<div className="demo-big-content">
 					{/* <h1>Hello, World!</h1> */}
 					{/* justifies incorrectly */}
-					<Layout>
-						<Header title="Mike Preston's Portfolio!" scroll>
-							<Navigation>
-								<Link to="/resume">Resume</Link>
-								<Link to="/aboutme">About Me</Link>
-								<Link to="/projects">Projects</Link>
-								<Link to="/contact">Contact</Link>
-							</Navigation>
-						</Header>
-						<Drawer title="Title">
-							<Navigation>
-                                <Link to="/resume">Resume</Link>
-                                <Link to="/aboutme">About Me</Link>
-                                <Link to="/projects">Projects</Link>
-                                <Link to="/contact">Contact</Link>
-							</Navigation>
-						</Drawer>
-						<Content>
-							<div className="page-content" />
-							{/* body goes here */}
-							<Main />
-						</Content>
-					</Layout>
+					{/* <Portfolio/> */}
 				</div>
-				{/* <Footer size="mini">
-					<FooterSection type="left" logo="Title">
-						<FooterLinkList>
-							<a href="/">Help</a>
-							<a href="/">Privacy & Terms</a>
-						</FooterLinkList>
-					</FooterSection>
-				</Footer> */}
+				
 			</body>
 		);
 	}
---
+++ src/pages/Portfolio/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Landing from './landing';
+import About from './about';
+import Projects from './projects';
+import Resume from './resume';
+import Contact from './contact';
+import { Layout, Header, Footer, FooterSection, FooterLinkList, Navigation, Drawer, Content } from 'react-mdl';
+import { Link } from 'react-router-dom';
+
+
+const Portfolio = () => (
+
+    <Layout>
+        <Header title="Mike Preston's Portfolio!" scroll>
+            <Navigation>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            {/* body goes here */}
+            {/* <Main /> */}
+            <Switch>
+                <Route exact path="/" component={Landing} />
+                <Route path="/aboutme" component={About} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/projects" component={Projects} />
+                <Route path="/resume" component={Resume} />
+            </Switch>
+        </Content>
+        {/* <Footer size="mini">
+                <FooterSection type="left" logo="Title">
+                    <FooterLinkList>
+                        <a href="/">Help</a>
+                        <a href="/">Privacy & Terms</a>
+                    </FooterLinkList>
+                </FooterSection>
+            </Footer> */}
+    </Layout>
+	
+);
+
+export default Portfolio;
