ed7cd9991666be84e19199fdcda448337dc4e0dd
Michael Preston <michael.n.preston@gmail.com>
2019-04-10 13:55:17 -0500

    removed all MDL (Material Design Lite)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material';
 
 ReactDOM.render(
   <Router>
