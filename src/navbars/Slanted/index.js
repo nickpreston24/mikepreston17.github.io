a7c4ee56af83b6b4e5b054c5eb78201b8f564660
Michael Preston <michael.n.preston@gmail.com>
2019-04-10 13:46:20 -0500

    working on ListItem (wrapped <li>) for links

--- src/navbars/Slanted/index.js
+++ src/navbars/Slanted/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
 
 const SlantedNavbar = () => (
 
@@ -10,11 +12,31 @@ const SlantedNavbar = () => (
             <i className="fa fa-bars"></i>
         </div>
         <div className="navigation-menu">
-            <Link to="/">HOME</Link>
-            <Link to="/projects">GALLERY</Link>{' '}
-            <Link to="/contact">CONTACT</Link>{' '}
-            <Link to="/aboutme">ABOUT</Link>{' '}
-            <Link to="/resume">RESUME</Link>{' '}
+
+            <ul>
+                {/* <li><Link to="/">HOME</Link></li>
+                <li><Link to="/projects">GALLERY</Link>{' '}</li>
+                <li><Link to="/contact">CONTACT</Link>{' '}</li>
+                <li><Link to="/aboutme">ABOUT</Link>{' '}</li>
+                <li><Link to="/resume">RESUME</Link>{' '}</li> */}
+
+                <Link to="/">HOME</Link>
+                <Link to="/projects">GALLERY</Link>{' '}
+                <Link to="/contact">CONTACT</Link>{' '}
+                <Link to="/aboutme">ABOUT</Link>{' '}
+                <Link to="/resume">RESUME</Link>{' '}
+
+                {/* <ListItem component={Link} to="/" button>HOME</ListItem>
+                <ListItem component={Link} to="/projects" button>GALLERY</ListItem>
+                <ListItem component={Link} to="/contact" button>CONTACT ME</ListItem> */}
+
+                <ListItem component={Link} to="/contact" button>CONTACT</ListItem>
+                <ListItem component={Link} to="/contact" button>CONTACT</ListItem>
+                <ListItem component={Link} to="/contact" button>CONTACT</ListItem>
+                <ListItem component={Link} to="/contact" button>CONTACT</ListItem>
+
+            </ul>
+
         </div>
     </div>
     <section className="hero">
