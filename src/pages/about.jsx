9cbbc90c4756c1073f791f9a57267d4e79795728
Michael Preston <michael.n.preston@gmail.com>
2019-04-13 14:51:26 -0500

    Rearranged & added MUI components

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,6 @@ import { withStyles } from '@material-ui/core';
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
 import Typography from "@material-ui/core/Typography";
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
