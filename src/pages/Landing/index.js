82504ec7a9b6322d77fa2266e128817b28b3d083
Michael Preston <michael.n.preston@gmail.com>
2019-04-17 23:05:45 -0500

    now withStyles!

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css'
+import { withStyles } from '@material-ui/core';
 
-const Landing = () => {
+const styles = theme => ({
+    span: {
+        fontSize: "65px",
+        "letter-spacing": "2px",
+        "font-family": "Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif"
+    }
+})
+
+const Landing = (props) => {
+
+    const { span } = props.classes;
 
     return (
         <>
-            <section className="hero container">
+            <section className={"hero container"}>
                 <div id="logo">MICHAEL PRESTON</div>
-                <h1>Full Stack Design <br></br> <span> PORTFOLIO</span></h1>
+                <h1>Full Stack Design <br></br> <span className={span}> PORTFOLIO</span></h1>
                 <div className="button">
                     <Link className="btn1" to="/projects">See Portfolio!</Link>
                     <a href="https://www.linkedin.com/in/michael-n-preston/" className="btn2">Connect on LI!</a>
@@ -17,4 +28,4 @@ const Landing = () => {
         </>)
 }
 
-export default Landing;
+export default withStyles(styles)(Landing);
