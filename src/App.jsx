70ef99db76b651a58d4e0c23fe2afc4b02b07c8d
Michael Preston <michael.n.preston@gmail.com>
2019-04-18 18:28:09 -0500

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Redirect, Switch, Route } from 'react-router-dom';
 import SlantedNavbar from './navbars/Slanted'
-
 import { About, Landing, Contact, Resume, Gallery } from './pages';
 
-// Font Awesome:
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faStroopwafel, faPlayCircle } from '@fortawesome/free-solid-svg-icons'
-
 import './App.css';
-
-// library.add(faStroopwafel, faPlayCircle)
 
 class App extends Component {
     render() {
